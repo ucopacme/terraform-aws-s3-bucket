d6865ee71eff44f8319d1fd4f6915e9edd05b8d0
David Rivera <David.Rivera@ucop.edu>
2020-10-05 12:12:39 -0700

    initial config

--- outputs.tf
+++ outputs.tf
@@ -41,7 +41,7 @@ output "enabled" {
 #output "user_arn" {
 #  value       = module.s3_user.user_arn
 #  description = "The ARN assigned by AWS for the user"
-}
+#}
 #
 #output "user_unique_id" {
 #  value       = module.s3_user.user_unique_id
