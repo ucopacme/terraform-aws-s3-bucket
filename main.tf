70d270df58db7f72900378a4905dce0fc8a13ed4
David Rivera <David.Rivera@ucop.edu>
2020-10-05 13:23:48 -0700

    initial config

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "this" {
 #  count         = module.this.enabled ? 1 : 0
   count      = var.enabled ? 1 : 0
-  #bucket        = module.this.id
-  bucket        = var.bucket
+  bucket        = module.this.id
+  #bucket        = var.bucket
   acl           = try(length(var.grants), 0) == 0 ? var.acl : null
   force_destroy = var.force_destroy
   policy        = var.policy
