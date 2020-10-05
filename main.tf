ff152f60044880277eddcdf37fb4bb5123c9d135
David Rivera <David.Rivera@ucop.edu>
2020-10-05 13:32:15 -0700

    initial config

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "this" {
 #  count         = module.this.enabled ? 1 : 0
   count      = var.enabled ? 1 : 0
-  bucket        = module.this.id
-  #bucket        = var.bucket
+ # bucket        = module.this.id
+  bucket        = var.bucket
   acl           = try(length(var.grants), 0) == 0 ? var.acl : null
   force_destroy = var.force_destroy
   policy        = var.policy
@@ -23,54 +23,54 @@ resource "aws_s3_bucket" "this" {
 
   }
 
-data "aws_partition" "current" {}
+#data "aws_partition" "current" {}
 
-data "aws_iam_policy_document" "bucket_policy" {
-  count = module.this.enabled && var.allow_encrypted_uploads_only ? 1 : 0
+#data "aws_iam_policy_document" "bucket_policy" {
+#  count = module.this.enabled && var.allow_encrypted_uploads_only ? 1 : 0
 
-  statement {
-    sid       = "DenyIncorrectEncryptionHeader"
-    effect    = "Deny"
-    actions   = ["s3:PutObject"]
-    resources = ["arn:${data.aws_partition.current.partition}:s3:::${join("", aws_s3_bucket.this.*.id)}/*"]
+#  statement {
+#    sid       = "DenyIncorrectEncryptionHeader"
+#    effect    = "Deny"
+#    actions   = ["s3:PutObject"]
+#    resources = ["arn:${data.aws_partition.current.partition}:s3:::${join("", aws_s3_bucket.this.*.id)}/*"]
+#
+#    principals {
+#      identifiers = ["*"]
+#      type        = "*"
+#    }
+#
+#    condition {
+#      test     = "StringNotEquals"
+#      values   = [var.sse_algorithm]
+#      variable = "s3:x-amz-server-side-encryption"
+#    }
+#  }
+#
+#  statement {
+#    sid       = "DenyUnEncryptedObjectUploads"
+#    effect    = "Deny"
+#    actions   = ["s3:PutObject"]
+#    resources = ["arn:${data.aws_partition.current.partition}:s3:::${join("", aws_s3_bucket.this.*.id)}/*"]
+#
+#    principals {
+#      identifiers = ["*"]
+#      type        = "*"
+#    }
+#
+#    condition {
+#      test     = "Null"
+#      values   = ["true"]
+#      variable = "s3:x-amz-server-side-encryption"
+#    }
+#  }
+#}
 
-    principals {
-      identifiers = ["*"]
-      type        = "*"
-    }
-
-    condition {
-      test     = "StringNotEquals"
-      values   = [var.sse_algorithm]
-      variable = "s3:x-amz-server-side-encryption"
-    }
-  }
-
-  statement {
-    sid       = "DenyUnEncryptedObjectUploads"
-    effect    = "Deny"
-    actions   = ["s3:PutObject"]
-    resources = ["arn:${data.aws_partition.current.partition}:s3:::${join("", aws_s3_bucket.this.*.id)}/*"]
-
-    principals {
-      identifiers = ["*"]
-      type        = "*"
-    }
-
-    condition {
-      test     = "Null"
-      values   = ["true"]
-      variable = "s3:x-amz-server-side-encryption"
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "this" {
-  count      = module.this.enabled && var.allow_encrypted_uploads_only ? 1 : 0
-  bucket     = join("", aws_s3_bucket.this.*.id)
-  policy     = join("", data.aws_iam_policy_document.bucket_policy.*.json)
-  depends_on = [aws_s3_bucket_public_access_block.this]
-}
+#resource "aws_s3_bucket_policy" "this" {
+#  count      = module.this.enabled && var.allow_encrypted_uploads_only ? 1 : 0
+#  bucket     = join("", aws_s3_bucket.this.*.id)
+#  policy     = join("", data.aws_iam_policy_document.bucket_policy.*.json)
+#  depends_on = [aws_s3_bucket_public_access_block.this]
+#}
 
 resource "aws_s3_bucket_public_access_block" "this" {
   count  = module.this.enabled ? 1 : 0
