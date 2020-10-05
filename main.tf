fe7ebd3b4c927c43187a7ec97b82c40f4c91365a
David Rivera <David.Rivera@ucop.edu>
2020-10-05 11:20:34 -0700

    initial config

---
+++ main.tf
@@ -0,0 +1,121 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+resource "aws_s3_bucket" "default" {
+  count         = module.this.enabled ? 1 : 0
+  bucket        = module.this.id
+  acl           = try(length(var.grants), 0) == 0 ? var.acl : null
+  force_destroy = var.force_destroy
+  policy        = var.policy
+  tags          = module.this.tags
+
+  versioning {
+    enabled = var.versioning_enabled
+  }
+
+  lifecycle_rule {
+    id                                     = module.this.id
+    enabled                                = var.lifecycle_rule_enabled
+    prefix                                 = var.prefix
+    tags                                   = var.lifecycle_tags
+    abort_incomplete_multipart_upload_days = var.abort_incomplete_multipart_upload_days
+
+    noncurrent_version_expiration {
+      days = var.noncurrent_version_expiration_days
+    }
+
+    dynamic "noncurrent_version_transition" {
+      for_each = var.enable_glacier_transition ? [1] : []
+
+      content {
+        days          = var.noncurrent_version_transition_days
+        storage_class = "GLACIER"
+      }
+    }
+
+    expiration {
+      days = var.expiration_days
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = var.sse_algorithm
+        kms_master_key_id = var.kms_master_key_arn
+      }
+    }
+  }
+
+  dynamic "grant" {
+    for_each = try(length(var.grants), 0) == 0 || try(length(var.acl), 0) > 0 ? [] : var.grants
+
+    content {
+      id          = grant.value.id
+      type        = grant.value.type
+      permissions = grant.value.permissions
+      uri         = grant.value.uri
+    }
+  }
+}
+
+data "aws_partition" "current" {}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  count = module.this.enabled && var.allow_encrypted_uploads_only ? 1 : 0
+
+  statement {
+    sid       = "DenyIncorrectEncryptionHeader"
+    effect    = "Deny"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:${data.aws_partition.current.partition}:s3:::${join("", aws_s3_bucket.default.*.id)}/*"]
+
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      values   = [var.sse_algorithm]
+      variable = "s3:x-amz-server-side-encryption"
+    }
+  }
+
+  statement {
+    sid       = "DenyUnEncryptedObjectUploads"
+    effect    = "Deny"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:${data.aws_partition.current.partition}:s3:::${join("", aws_s3_bucket.default.*.id)}/*"]
+
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    condition {
+      test     = "Null"
+      values   = ["true"]
+      variable = "s3:x-amz-server-side-encryption"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "default" {
+  count      = module.this.enabled && var.allow_encrypted_uploads_only ? 1 : 0
+  bucket     = join("", aws_s3_bucket.default.*.id)
+  policy     = join("", data.aws_iam_policy_document.bucket_policy.*.json)
+  depends_on = [aws_s3_bucket_public_access_block.default]
+}
+
+resource "aws_s3_bucket_public_access_block" "default" {
+  count  = module.this.enabled ? 1 : 0
+  bucket = join("", aws_s3_bucket.default.*.id)
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
