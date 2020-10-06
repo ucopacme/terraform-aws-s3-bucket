b92c15d49894c49cc25600a4173d503808279a49
Eric Odell <eric.odell@ucop.edu>
2020-10-05 21:06:11 -0700

    example

---
+++ example/.terragrunt-cache/bdnNeSXrjwdV-DpMdAYm9my03Xw/aiwsLfyL2aQ5-OGIskDfAE-OAw0/main.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "this" {
+  acl    = var.acl
+  bucket = var.bucket
+  count      = var.enabled ? 1 : 0
+  force_destroy = var.force_destroy
+  tags       = merge(var.tags, map("Name", var.bucket))
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  bucket = join("",aws_s3_bucket.this.*.id)
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
