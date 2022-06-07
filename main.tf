3339008b8966bb885f407c700ab839aa1304ebdc
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-06-07 11:08:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,42 @@
 resource "aws_s3_bucket" "this" {
-  acl           = var.acl
   bucket        = var.bucket
   count         = var.enabled ? 1 : 0
   force_destroy = var.force_destroy
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = var.sse_algorithm
-        kms_master_key_id = var.kms_master_key_arn
-      }
+  tags = merge(var.tags, map("Name", var.bucket))
+}
+#
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = join("", aws_s3_bucket.this.*.id)
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_master_key_arn
+      sse_algorithm     = var.sse_algorithm
     }
   }
-  tags = merge(var.tags, map("Name", var.bucket))
-  versioning {
-    enabled = var.versioning_enabled
+}
+#
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = join("", aws_s3_bucket.this.*.id)
+  versioning_configuration {
+    status = var.versioning_enabled
   }
 }
-
+#
+resource "aws_s3_bucket_acl" "this" {
+  bucket = join("", aws_s3_bucket.this.*.id)
+  acl    = var.acl
+}
 resource "aws_s3_bucket_public_access_block" "this" {
   block_public_acls       = var.block_public_acls
   block_public_policy     = var.block_public_policy
   bucket                  = join("", aws_s3_bucket.this.*.id)
-  count                   = var.enabled ? 1 : 0
   ignore_public_acls      = var.ignore_public_acls
   restrict_public_buckets = var.restrict_public_buckets
 }
 
 resource "aws_s3_bucket_policy" "this" {
-  count  = var.enabled ? 1 : 0
+  count  = var.policy_enabled ? 1 : 0
   bucket = join("", aws_s3_bucket.this.*.id)
   policy = var.policy
 }
